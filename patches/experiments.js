1ce1dab369c58db60c1b241cea00dbe1c693b92d
eternal <eternal.git@gmail.com>
2022-08-03 19:09:43 +0100

    fix experiments

--- patches/experiments.js
+++ patches/experiments.js
@@ -1,4 +1,5 @@
 import Webpack from '../modules/webpack.js';
+import Patcher from '../modules/patcher.js';
 
 export default {
    displayName: 'Experiments',
@@ -6,22 +7,57 @@ export default {
    executor: async () => {
       await Webpack.whenReady;
 
-      const Experiments = Webpack.findByProps('isDeveloper');
-      Experiments._isDeveloper = Experiments.isDeveloper;
-      Object.defineProperty(Experiments, 'isDeveloper', {
-         get: () => true,
-         configurable: true
-      });
+      const Dispatcher = Webpack.findByProps('_dispatch', 'dispatch');
+      const Users = Webpack.findByProps('getCurrentUser', 'getUser');
 
-      for (let i = 0; i < Experiments._changeCallbacks.listeners.length; i++) {
-         Experiments._changeCallbacks.listeners[i]();  
+      // Wait for dispatcher handlers
+      const events = Dispatcher._orderedActionHandlers;
+      while (!events.CONNECTION_OPEN) {
+         await new Promise(r => setTimeout(r, 10));
       }
 
-      return () => {
-         Experiments.isDeveloper = Experiments._isDeveloper;
-         for (let i = 0; i < Experiments._changeCallbacks.listeners.length; i++) {
-            Experiments._changeCallbacks.listeners[i]();  
+      // Grab handlers for CONNECTION_OPEN
+      const Handlers = events['CONNECTION_OPEN'];
+
+      // Spoof the staff flag
+      const unpatch = Patcher.after('experiments', Users, 'getCurrentUser', (_, __, res) => {
+         if (!res) return;
+
+         return new Proxy({}, {
+            get(_, prop) {
+               if (prop === 'hasFlag') {
+                  return function (flag) {
+                     if (flag === 1) {
+                        return true;
+                     }
+
+                     return res.hasFlag.call(this, flag);
+                  };
+               }
+
+               return res[prop];
+            }
+         });
+      });
+
+      // Call the dispatcher action handler with the spoofed flags to internally allow bucket overrides
+      const ExperimentStore = Handlers.find(h => h.name === 'ExperimentStore');
+      if (ExperimentStore) ExperimentStore.actionHandler({
+         type: 'CONNECTION_OPEN',
+         guildExperiments: [],
+         experiments: [],
+         user: {
+            ...Users.getCurrentUser(),
+            flags: 1,
          }
-      };
+      });
+
+      // Call the dispatcher action handler to update isDeveloper internally
+      const DeveloperExperimentStore = Handlers.find(h => h.name === 'DeveloperExperimentStore');
+      if (DeveloperExperimentStore) DeveloperExperimentStore.actionHandler();
+
+      unpatch();
+
+      return () => Patcher.unpatchAll('experiments');
    }
 };
